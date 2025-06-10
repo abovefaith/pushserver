d688b7a796bca0a649886ad17ba204bb002202be
Faith Kawira <faith.kawira@kamsware.com>
2025-06-10 17:09:51 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const webpush = require('web-push');
+
+console.log(webpush.generateVAPIDKeys());
+
+const publickKey = 'BMvwQtRr6Ycta1xvs747FHYnJG9-xHfA04LHeAMiwClpRNfPU2T0DFw8IJp67uuHtODbMHad8io0syXVN0rcOJc';
+const privateKey = 'Nx5V-kiq9MToSI47v55gvcqyJHm-c3OnNW5lVwVG79k';
+
+
